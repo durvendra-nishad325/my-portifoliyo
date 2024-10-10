52f753e5a1eab1cdc39be4e4e70c834038f7e974
durvendra-nishad325 <156454768+durvendra-nishad325@users.noreply.github.com>
2024-10-10 12:19:08 +0530

    i ahve added all boilers in git

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import AppRoutes from './routes/AppRoutes';
+
+function App() {
+  return (
+    <>
+    <p>hello</p> 
+    <AppRoutes/>
+    </>
+  );
+}
+
+export default App;
